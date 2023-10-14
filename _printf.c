e4e358dacec9088e8f1b9888e737e3d46f277207
Moealsir <mohamedwdalsir@gmail.com>
2023-10-14 15:25:13 +0300

    betty done

--- _printf.c
+++ _printf.c
@@ -1,36 +1,48 @@
 #include "main.h"
 
-int _printf(const char *format, ...) {
-    int count = 0;
-    
-    va_list args;
-    va_start(args, format);
+/**
+ * _printf - our magical printf function
+ * @format: the inserted format
+ * Return: count
+ */
 
-    while (*format != '\0') {
-        if (*format == '%') {
-            switch (*(++format)) {
-                case 'c':
-                    count += _putchar(va_arg(args, int));
-                    break;
-                case 's':
-                    count += _puts(va_arg(args, char *));
-                    break;
-                case 'd':
-                case 'i':
-                    count += printf("%d", va_arg(args, int));
-                    break;
-                case '%':
-                    count += _putchar('%');
-                    break;
-                default:
-                    return -1;
-            }
-        } else {
-            count += _putchar(*format);
-        }
-        format++;
-    }
+int _printf(const char *format, ...)
+{
+	int count = 0;
+	va_list args;
 
-    va_end(args);
-    return count;
+	va_start(args, format);
+
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			switch (*(++format))
+			{
+				case 'c':
+					count += _putchar(va_arg(args, int));
+					break;
+				case 's':
+					count += _puts(va_arg(args, char *));
+					break;
+				case 'd':
+				case 'i':
+					count += printf("%d", va_arg(args, int));
+					break;
+				case '%':
+					count += _putchar('%');
+					break;
+				default:
+					return (-1);
+			}
+		}
+		else
+		{
+			count += _putchar(*format);
+		}
+		format++;
+	}
+
+	va_end(args);
+	return (count);
 }
