8e9b0ffa0aff1173ff4a72bff3eefec80f66fbb3
Moealsir <mohamedwdalsir@gmail.com>
2023-10-14 18:38:34 +0300

    testing

---
+++ README.md
@@ -0,0 +1,10 @@
+**#Specifiers handled**
+
+1. s
+2. c
+3. %
+4. b
+5. d
+
+**#Specifiers stuck with**
+1. i
--- _printf.c
+++ _printf.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdarg.h>
 
+/**README first*/
 /**
  * handlies specifier untill now:
  * 1. s
@@ -14,6 +15,7 @@
  * @format: the inserted format
  * Return: count
  */
+
 int _printf(const char *format, ...)
 {
     int count = 0;
@@ -76,39 +78,6 @@ int I_handle_d_specifier(int n)
         count += I_handle_d_specifier(n / 10);
     }
     return (count + _putchar(n % 10 + '0'));
-}
-
-/**
- * I_handle_b_specifier - handle the %b specifier.
- * @n: integer
- * Return: count + binary representation
- */
-int I_handle_b_specifier(int n)
-{
-    int count = 0;
-    int leadingZero = 1;
-    int i;
-
-    for (i = 31; i >= 0; i--)
-    {
-        char bit = (n >> i) & 1;
-        if (bit == 1)
-        {
-            leadingZero = 0;
-        }
-
-        if (!leadingZero)
-        {
-            count += _putchar(bit + '0');
-        }
-    }
-
-    if (count == 0)
-    {
-        count += _putchar('0');
-    }
-
-    return (count);
 }
 
 /**
